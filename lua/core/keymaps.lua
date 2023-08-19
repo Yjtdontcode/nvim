7fdc2a370d51a966d0909bf5f4b4f34aefcef0a3
Lepizi <yjtpost@qq.com>
2023-08-19 22:09:50 +0800

    初始化git仓库，初步完成‘init.lua', 'keymaps.lua', 'options.lua'

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,30 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- ---------- 插入模式 ---------- ---
+keymap.set("i", "jk", "<ESC>")
+keymap.set("i", "kj", "<ESC>")
+
+-- ---------- 视觉模式 ---------- ---
+-- 单行或多行移动
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- ---------- 正常模式 ---------- ---
+-- 窗口
+keymap.set("n", "<leader>sv", "<C-w>v") -- 水平新增窗口 
+keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
+-- 文件操作
+keymap.set("n", "<leader>ww", "<cmd>w<CR>") -- 写入文件
+keymap.set("n", "<leader>qq", "<cmd>q<CR>") -- 退出文件
+keymap.set("n", "<leader>w0", "<cmd>w!<CR>") -- 强制写入文件
+keymap.set("n", "<leader>q0", "<cmd>q!<CR>") -- 强制退出文件
+keymap.set("n", "<leader>wq", "<cmd>wq<CR>") -- 写入并退出文件
+keymap.set("n", "<leader>so", "<cmd>so<CR>") -- 重启文件
+-- 光标移动
+keymap.set("n", "<leader>hh", "^")
+keymap.set("n", "<leader>ll", "$")
+
+-- 取消高亮
+keymap.set("n", "<leader>nh", ":nohl<CR>")
